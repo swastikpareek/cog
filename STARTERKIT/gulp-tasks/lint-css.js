be239e7009f77932f26d8b5fc9c9ad3a895d6bff
Bassam Ismail <skippednote@gmail.com>
2017-04-12 23:02:16 +0530

    Add babel support to transform ES2015.

--- STARTERKIT/gulp-tasks/lint-css.js
+++ STARTERKIT/gulp-tasks/lint-css.js
@@ -12,7 +12,7 @@ module.exports = function (gulp, plugins, options) {
   gulp.task('lint:css', function () {
     return gulp.src(options.sass.files)
       .pipe(plugins.plumber())
-      .pipe(plugins.gulpStylelint({
+      .pipe(plugins.stylelint({
         reporters: [
           {
             formatter: 'string',
@@ -26,7 +26,7 @@ module.exports = function (gulp, plugins, options) {
   // Lint scss files and throw an error for a CI to catch.
   gulp.task('lint:css-with-fail', function () {
     return gulp.src(options.sass.files)
-      .pipe(plugins.gulpStylelint({
+      .pipe(plugins.stylelint({
         reporters: [
           {
             formatter: 'string',
